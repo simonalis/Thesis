c5b60455839cea579b11d13e2502839291c74a9c
simonal <simona.lisker@gmail.com>
2022-11-18 16:14:44 +0200

    1. In the middle of roberta data loader

--- src/LoadData.py
+++ src/LoadData.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import fragment_creation
 import model_generation_all_fifty
-
+import sklearn.utils as sk
 #data_512_4 = "/512_4/"
 train_base_path = fragment_creation.absolute_path + "/" + fragment_creation.data_set_type + "/"
 
@@ -12,23 +12,24 @@ def load_dataset(data_dir):
     train_data = np.load(os.path.join(data_dir, 'train.npz'), mmap_mode='r')
     train_data = train_data
     x_train, y_train = train_data['x'], train_data['y']
+    x_train, y_train = sk.shuffle(x_train, y_train, random_state=10)
     # one_hot_y_train = to_categorical(y_train)
 
     print("Training Data loaded with shape: {} and labels with shape - {}".format(x_train.shape,
                                                                                   y_train.shape))
-    x_train, y_train = x_train[:100000], y_train[:100000]
+   # x_train, y_train = x_train[:1000000], y_train[:1000000]
 
     print("Training Data loaded with shape: {} and labels with shape - {}".format(x_train.shape,
                                                                                   y_train.shape))
     val_data = np.load(os.path.join(data_dir, 'val.npz'), mmap_mode='r')
     val_data = val_data
     x_val, y_val = val_data['x'], val_data['y']
-    x_val, y_val = x_val[:20000], y_val[:20000]
+    #x_val, y_val = x_val[:300000], y_val[:300000]
     # one_hot_y_val = to_categorical(y_val)
     print("Validation Data loaded with shape: {} and labels with shape - {}".format(x_val.shape, y_val.shape))
     test_data = np.load(os.path.join(data_dir, 'test.npz'), mmap_mode='r')
     x_test, y_test = test_data['x'], test_data['y']
-    x_test, y_test = x_test[:20000], y_test[:20000]
+    #x_test, y_test = x_test[:300000], y_test[:300000]
     # one_hot_y_test = to_categorical(y_val)
     print("Testing Data loaded with shape: {} and labels with shape - {}".format(x_test.shape, y_test.shape))
     return x_train, y_train, x_val, y_val, x_test, y_test
